c8ca80fdd3a47e2d6e22b72c445dbd5ca8be5d79
Shankarnag151 <shankarnag151@gmail.com>
2018-07-09 10:59:36 +0530

    Hello

--- fb_app.py
+++ fb_app.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 user = "shankarnag151@gmail.com"
 pwd = "9980390042"
 driver = webdriver.Chrome()
-dr.maximize_window()
+driver.maximize_window()
 driver.get("http://www.facebook.com")
 assert "Facebook" in driver.title
 elem = driver.find_element_by_id("email")
