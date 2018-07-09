433833958484d25d9678b32e926f48372bb25dc9
Shankarnag151 <shankarnag151@gmail.com>
2018-07-09 10:56:43 +0530

    Hello

--- fb_app.py
+++ fb_app.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-user = "username"  # Username for FB A/c.
-pwd = "password"   # Password for FB A/c.
+user = "shankarnag151@gmail.com"
+pwd = "9980390042"
 driver = webdriver.Chrome()
+dr.maximize_window()
 driver.get("http://www.facebook.com")
 assert "Facebook" in driver.title
 elem = driver.find_element_by_id("email")
