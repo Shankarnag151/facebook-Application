6e4af1895c6316a7a28d2d043d02c57c431250cf
Shankarnag <34434605+omshankar555@users.noreply.github.com>
2018-08-31 14:43:50 +0530

    new update..

--- fb_app.py
+++ fb_app.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 user = "FB Username"       # Enter your credentials to login FB..
 pwd = "FB Password"
+
 driver = webdriver.Chrome()
 driver.maximize_window()
+
 driver.get("http://www.facebook.com")
 assert "Facebook" in driver.title
+
 elem = driver.find_element_by_id("email")
 elem.send_keys(user)
 elem = driver.find_element_by_id("pass")
 elem.send_keys(pwd)
+
 elem.send_keys(Keys.RETURN)
 
 elem.driver.implicitly_wait(5)
